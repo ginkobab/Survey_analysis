73eae24a9d9e1e28adfa3c866d1e66350c047bdb
ginko-ezbook <ginkobab@gmail.com>
2019-11-27 16:42:35 +0100

    auto-commit

--- cleaner.py
+++ cleaner.py
@@ -2,26 +2,35 @@
 
 import pandas as pd
 
-file_in = input("Scrivi il path del questionario da pulire!   ")
 
-raw = pd.read_csv(file_in)
 
-useless_features = ['Status',
-                    'IPAddress',
-                    'Progress',
-                    'Duration (in seconds)',
-                    'RecordedDate',
-                    'ResponseId',
-                    'RecipientLastName',
-                    'RecipientFirstName',
-                    'RecipientEmail',
-                    'ExternalReference',
-                    'LocationLatitude',
-                    'LocationLongitude',
-                    'DistributionChannel',
-                    'UserLanguage']
+def remove_features(data):
+    useless_features = ['StartDate',
+                        'EndDate',
+                        'Status',
+                        'IPAddress',
+                        'Progress',
+                        'RecordedDate',
+                        'ResponseId',
+                        'RecipientLastName',
+                        'RecipientFirstName',
+                        'RecipientEmail',
+                        'ExternalReference',
+                        'LocationLatitude',
+                        'LocationLongitude',
+                        'DistributionChannel',
+                        'UserLanguage']
 
-raw = raw.drop(useless_features, axis=1)
-raw = raw.drop([0, 1], axis=0)
+    data = data.drop(useless_features, axis=1)
+    data = data.drop([0, 1], axis=0)
+    return data
 
-raw.to_csv('clean_' + file_in, index=False)
+
+
+
+if __name__ == '__main__':
+
+    file_in = input("Scrivi il path del questionario da pulire: ")
+    raw = pd.read_csv(file_in)
+    new = remove_features(raw)
+    new.to_csv('clean_' + file_in, index=False)
